47bb2032f8d342abf31842a8f8eacabee2351e35
Anthony K <antonyk@users.noreply.github.com>
2020-06-17 19:28:33 -0400

    moving zeroes

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -3,9 +3,22 @@ Input: a List of integers
 Returns: a List of integers
 '''
 def moving_zeroes(arr):
-    # Your code here
+    # iterate over the array
+    # if we find a zero, swap with the last non-zero value in the array
+    last = len(arr)-1
+    cur = 0
 
-    pass
+    while cur < last:
+        if arr[cur] == 0:
+            while arr[last] == 0 and cur < last:
+                last -= 1
+            if arr[last] != 0:
+                arr[cur], arr[last] = arr[last], arr[cur]
+            else:
+                break
+        cur += 1
+
+    return arr
 
 
 if __name__ == '__main__':
